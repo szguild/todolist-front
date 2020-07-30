17ce2b0f2d5de381414ba33c57fe7dff6f1e189e
mj.lee <mj.lee@homeplus.co.kr>
2020-07-30 11:04:22 +0900

    add local project

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <header>
+      <h2 calss="container">Todo</h2>
+    </header>
+
+    <div calss="container">
+      <add-form v-bind:value="query" v-on:@submit="onInputTodo"></add-form>
+      <!-- FormComponent.vue에서 inputValue에 넣어준 value를 v-bind가 App.vue의 query와 바인딩해줌 -->
+      <!-- v-on:@submit => FormComponent.vue에서 $emit으로 전달한 @submit이라는 이벤트를 받아와서 onInputTodo 메서드와 연결 -->
+      <tab v-bind:tabs="tabs" v-bind:selected-tab="selectedTab" v-on:@change="onClickTab"></tab>
+    </div>
+
+    <div>
+      <list v-bind:selected-tab="selectedTab" v-bind:data="todoList"
+        v-on:@finish="onClickFinish"
+        v-on:@reset="onClickReset"></list>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import ToDoForm from './components/ToDoForm'
+import ToDoTab from './components/ToDoTab'
+import ToDoList from './components/ToDoList'
+import ToDoModel from './model/ToDoModel'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      query: '',
+      tabs: ['todo', 'finish'],
+      selectedTab: '',
+      todoList: []
+    }
+  },
+  created () { // vue 인스턴스가 생성된 후에 실행됨
+    this.selectedTab = this.tabs[0] // todo 탭 선택
+    this.search() // todo list 출력
+  },
+  components: { // 사용할 컴포넌트 등록
+    'add-form': ToDoForm,
+    'tab': ToDoTab,
+    'list': ToDoList
+  },
+  methods: {
+    search () { // list 검색
+      ToDoModel.list(this.selectedTab).then(data => {
+        this.todoList = data
+      })
+    },
+    onClickTab (tab) { // tab 선택
+      this.selectedTab = tab
+      this.search()
+    },
+    onClickFinish (item) { // todo 완료
+      ToDoModel.finish(item)
+      this.search()
+    },
+    onClickReset (item) { // 완료된 todo 리셋
+      ToDoModel.reset(item)
+      this.search()
+    },
+    onInputTodo (query) { // todo 입력
+      ToDoModel.add(query)
+      this.selectedTab = this.tabs[0]
+      this.search()
+    }
+  }
+}
+
+</script>
---
+++ src/components/ToDoTab.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="tabs">
+    <li v-for="tab in tabs" :key="tab" v-bind:class="{active: tab === selectedTab}" v-on:click="onClickTabs(tab)">
+      {{tab}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['tabs', 'selectedTab'], // 부모(App.vue)로부터 받아옴 => tabs(todo, finish), selectedTab : 현재 선택한 탭
+  methods: {
+    onClickTabs (tab) {
+      // $emit => 부모(App.vue)에게 @change 이름으로 이벤트 전달
+      // tab(선택한 탭) 인자로 같이 전달
+      this.$emit('@change', tab)
+    }
+  }
+}
+</script>
